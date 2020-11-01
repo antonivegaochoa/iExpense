78d93dcb4a1678b08c7518b8a51a3b74ef84854e
antonivegaochoa <tonivegaochoa@gmail.com>
2020-11-01 16:45:21 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Antonio Vega on 11/1/20.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
